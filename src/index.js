cc6f32d938725efa46594f705081a1a7249a233e
ajlic24@gmail.com <ajlic24@gmail.com>
2021-12-07 15:51:53 -0500

    Done with list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector(`#submit`).addEventListener(`click`, (e) => {
+    e.preventDefault()
+    const form = document.querySelector(`#create-task-form`)
+    const input = document.querySelector('#new-task-description').value
+    let ul = document.querySelector(`#tasks`)
+    let li = document.createElement('li')
+    let btn = document.createElement(`button`)
+    btn.addEventListener(`click`, () => {
+      btn.parentNode.remove()
+    })
+    btn.style.backgroundColor = `pink`
+    btn.textContent = `X`
+    li.textContent = `${input} `
+    li.appendChild(btn)
+    ul.appendChild(li) 
+    form.reset()
+  })
+
+
 });
+
+
+
